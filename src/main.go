84421c2cca4d3f7a61b5f637d01d0e9e58379b8a
IJMAdalenA <IJMadalenA@outlook.com>
2023-07-09 18:47:53 +0200

    update main.go.

--- src/main.go
+++ src/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World.")
+	constant()
+}
+
+func constant() {
+	// Declare a constant.
+	const pi_1 float64 = 3.14
+	const pi_2 = 3.14
+	fmt.Println("pi_1:", pi_1)
+	fmt.Println("pi_2:", pi_2)
 }
