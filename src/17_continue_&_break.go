c09ed87e4c878d1de6790de6e360df84d66bb0a0
IJMAdalenA <IJMadalenA@outlook.com>
2023-07-22 11:42:27 +0200

    continue and break.

--- src/17_continue_&_break.go
+++ src/17_continue_&_break.go
@@ -1,1 +1,23 @@
 package main
+
+func main() {
+	// continue statement.
+	// The continue statement is used to skip the current iteration of the for loop, and the next iteration will be executed.
+	println("continue statement.")
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			continue
+		}
+		println(i)
+	}
+
+	// break statement.
+	// the break statement is used to terminate the for loop, switch statement, etc.
+	println("break statement.")
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			break
+		}
+		println(i)
+	}
+}
