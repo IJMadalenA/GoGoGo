e2907a59ea3f630b9548348c09c8e81c84944194
IJMAdalenA <IJMadalenA@outlook.com>
2023-07-23 21:27:40 +0200

    defer.

--- src/17_defer.go
+++ src/17_defer.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	// defer. It will be executed the function after the function is finished.
 	// It is used to close the file, close the connection, etc.
+	// defer <expression>
 	fmt.Println("start")
 	defer fmt.Println("middle")
 	fmt.Println("end")
