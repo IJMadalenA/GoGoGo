521933b17c5654e69156783414a4a36d1d9efd72
IJMAdalenA <IJMadalenA@outlook.com>
2023-07-23 21:28:11 +0200

    palindrome.

--- src/19_palindrome.go
+++ src/19_palindrome.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(text string) {
+	var textReverse string
+
+	// normalise the text.
+	text = strings.ToLower(text)
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+	if text == textReverse {
+		fmt.Println("The text is a palindrome.")
+	} else {
+		fmt.Println("The text is not a palindrome.")
+	}
+}
+
+func main() {
+	isPalindrome("kayak")
+}
