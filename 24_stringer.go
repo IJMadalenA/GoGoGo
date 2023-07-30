c6e4d503509d75beab7997ecfac22882c2d39c36
IJMAdalenA <IJMadalenA@outlook.com>
2023-07-30 03:26:16 +0200

    stringer.

--- 24_stringer.go
+++ 24_stringer.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+type guitar struct {
+	brand string
+	model string
+	price int
+}
+
+func (myGuitar guitar) String() string {
+	return fmt.Sprintf("%v model %v - price: $%v", myGuitar.brand, myGuitar.model, myGuitar.price)
+}
+
+func main() {
+	myGuitar := guitar{brand: "Gibson", model: "Les Paul", price: 3500}
+	fmt.Println("myGuitar:", myGuitar)
+}
