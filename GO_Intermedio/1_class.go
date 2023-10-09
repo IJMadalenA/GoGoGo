d68f42ad6ea417bd4cfc4fbc914a3ccccb274df7
IJMAdalenA <IJMadalenA@outlook.com>
2023-10-09 19:18:18 +0200

    update clases.

--- GO_Intermedio/1_class.go
+++ GO_Intermedio/1_class.go
@@ -3,8 +3,19 @@ package main
 import "fmt"
 
 type Employee struct {
+	// This is a field declaration. It's a variable that's part of a struct.
 	id   int
 	name string
+}
+
+func (e *Employee) SetId(id int) {
+	// This is a pointer receiver method. It can modify the value to which the receiver points.
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	// This is a value receiver method. It can't modify the value to which the receiver points.
+	e.name = name
 }
 
 func main() {
@@ -12,5 +23,9 @@ func main() {
 	fmt.Printf("%v\n", e)
 	e.id = 1
 	e.name = "John"
+	e.SetId(2)
+	fmt.Printf("%v\n", e)
+	e.SetId(3)
+	e.SetName("Jane")
 	fmt.Printf("%v\n", e)
 }
