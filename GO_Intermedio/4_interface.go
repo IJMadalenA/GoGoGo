52865feb151d64c5684355c96aedb0e3451bd090
IJMAdalenA <IJMadalenA@outlook.com>
2023-10-11 03:03:59 +0200

    interface 1.

--- GO_Intermedio/4_interface.go
+++ GO_Intermedio/4_interface.go
@@ -1,1 +1,46 @@
 package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+}
+
+type Employee3 struct {
+	ID int
+}
+
+type FullTimeEmployee struct {
+	Person
+	Employee3
+	taxRate int
+}
+
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full time employee"
+}
+
+type TemporaryEmployee struct {
+	Person
+	Employee3
+	taxRate int
+}
+
+func (tEmployee TemporaryEmployee) getMessage() string {
+	return "Temporary employee"
+}
+
+type PrintInfo interface {
+	getMessage() string
+}
+
+func getMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
+}
+
+func main() {
+	ftEmployee := FullTimeEmployee{}
+	tEmployee := TemporaryEmployee{}
+	getMessage(ftEmployee)
+	getMessage(tEmployee)
+}
