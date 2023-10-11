323626e432ac20007f0105a3d98218c4f5d752e9
IJMAdalenA <IJMadalenA@outlook.com>
2023-10-11 03:23:21 +0200

    interface 2.

--- GO_Intermedio/5_interface_abstract_factory.go
+++ GO_Intermedio/5_interface_abstract_factory.go
@@ -1,1 +1,35 @@
 package main
+
+import "fmt"
+
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending notification via SMS")
+}
+
+func (SMSNotificationSender) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+type SMSNotificationSender struct {
+}
+
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
+}
