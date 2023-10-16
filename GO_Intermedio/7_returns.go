9594a0507924d3146fe31ec8543f305f8a044b90
IJMAdalenA <IJMadalenA@outlook.com>
2023-10-16 08:06:17 +0200

    returns.

--- GO_Intermedio/7_returns.go
+++ GO_Intermedio/7_returns.go
@@ -14,8 +14,28 @@ func sum_var(values ...int) int {
 	return total
 }
 
+func printNames(names ...string) {
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+func getValues(x int) (int, int, int) {
+	return x, x * 2, x * 3
+}
+
+func getValues2(x int) (double int, triple int, quadruple int) {
+	double = x * 2
+	triple = x * 3
+	quadruple = x * 4
+	return
+}
+
 func main() {
 
 	fmt.Println(sum(5, 5))
 	fmt.Println(sum_var(5, 5, 5, 5, 5))
+	printNames("Juan", "Pedro", "Pablo")
+	fmt.Println(getValues(5))
+	fmt.Println(getValues2(5))
 }
