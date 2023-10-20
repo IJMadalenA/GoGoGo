3559b98243da4fdb337dd63ed26aceb99d1120db
IJMAdalenA <IJMadalenA@outlook.com>
2023-10-20 03:11:49 +0200

    test.

--- GO_Intermedio/testing/main_test.go
+++ GO_Intermedio/testing/main_test.go
@@ -8,3 +8,22 @@ func TestSum(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
+
+func TestSum2(t *testing.T) {
+	tables := []struct {
+		a int
+		b int
+		c int
+	}{
+		{1, 1, 2},
+		{2, 2, 4},
+		{3, 3, 6},
+	}
+
+	for _, item := range tables {
+		total := Sum(item.a, item.b)
+		if total != item.c {
+			t.Errorf("Sum was incorrect, got: %d, want: %d.", total, item.c)
+		}
+	}
+}
